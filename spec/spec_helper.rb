2a7d3c0689f17a55b1e38d6485dd796c15522ace
Graeme Nelson <graeme.nelson@gmail.com>
2016-08-20 15:00:00 -0700

    removed required securities on the main lib class, moved to spec since it is only required from specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,7 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 $:.unshift File.dirname(__FILE__) + '/../lib'
 require 'indicators'
+require 'securities'
 
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
